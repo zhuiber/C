896a44dc60c15ca357ab04706dec4bb81dc4abb3
zhuiber <2022300013@email.szu.edu.cn>
2024-05-31 14:54:53 +0800

    new file:   .vscode/tasks.json 	new file:   8-10.c 	new file:   8-10.exe 	modified:   chapter8_function/8-9.c 	new file:   chapter8_function/8-9.exe

--- 8-10.c
+++ 8-10.c
@@ -1,18 +1,21 @@
-// 求N的阶乘
+// 组合数计算
 
 #include <stdio.h>
 #define N 10
+#define M 5
 
+int C(int n, int m);
 long factorial(int n);
 
 int main()
 {
-  int i;
-  for (i = 0; i <= N; i++)
-  {
-    printf("%d! = %ld\n", i, factorial(i));
-  }
+  printf("C(%d, %d) = %d\n", N, M, C(N, M));
   return 0;
+}
+
+int C(int n, int m)
+{
+  return factorial(n) / (factorial(m) * factorial(n - m));
 }
 
 long factorial(int n)
