f1a5373540abe84d3109938620221ea83073e05f
zhuiber <2022300013@email.szu.edu.cn>
2024-06-13 09:12:27 +0800

    modified:   exercise5/hw1.exe 	modified:   exercise5/hw2.exe 	modified:   exercise5/hw3.c 	modified:   exercise5/hw3.exe 	modified:   exercise5/hw4.exe 	modified:   exercise5/hw5.exe 	modified:   exercise5/hw6.exe 	modified:   exercise5/hw7.exe 	modified:   exercise5/hw8.c 	new file:   exercise5/hw8.exe

--- exercise5/hw3.c
+++ exercise5/hw3.c
@@ -17,7 +17,7 @@ int main()
         printf("%d%d", num3, num2);
     }
     else
-      printf("%d%d%d", num3, num2, num1);
+      printf("%d%d%d", num3, num1, num2);
   else if (num2 < num3)
   {
     printf("%d", num2);
--- exercise5/hw8.c
+++ exercise5/hw8.c
@@ -4,3 +4,45 @@
 // 能被其中一个数(要指出哪一个) 整除;
 // 不能被3 .5, 7任一个整除。
 // 【设计思想】判断x被n整除可用x % n == 0来判断即可
+
+#include <stdio.h>
+
+int main()
+{
+  int x;
+  printf("Please enter an integer: ");
+  scanf("%d", &x);
+  if (x % 3 == 0 && x % 5 == 0 && x % 7 == 0)
+  {
+    printf("The integer can be divided by 3, 5, 7.\n");
+  }
+  else if (x % 3 == 0 && x % 5 == 0)
+  {
+    printf("The integer can be divided by 3 and 5.\n");
+  }
+  else if (x % 3 == 0 && x % 7 == 0)
+  {
+    printf("The integer can be divided by 3 and 7.\n");
+  }
+  else if (x % 5 == 0 && x % 7 == 0)
+  {
+    printf("The integer can be divided by 5 and 7.\n");
+  }
+  else if (x % 3 == 0)
+  {
+    printf("The integer can be divided by 3.\n");
+  }
+  else if (x % 5 == 0)
+  {
+    printf("The integer can be divided by 5.\n");
+  }
+  else if (x % 7 == 0)
+  {
+    printf("The integer can be divided by 7.\n");
+  }
+  else
+  {
+    printf("The integer can't be divided by 3, 5, 7.\n");
+  }
+  return 0;
+}
